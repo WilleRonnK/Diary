b88b9092da4d96367cc7d94a0082f06907db8a08
WilleRonnK <willerkjellqvist@gmail.com>
2022-10-30 04:13:26 +0100

    Final commit

--- src/main/java/Main/java/Main.java
+++ src/main/java/Main/java/Main.java
@@ -2,6 +2,7 @@ package Main.java;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -13,11 +14,11 @@ import static java.awt.SystemColor.text;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int choices = 0;
-        String userInput;
         ObjectMapper mapper = new ObjectMapper();
+        Scanner scanner = new Scanner(System.in);
         List<UserEntry> userEntries = List.of(mapper.readValue(Paths.get("src/main/resources/user.json").toFile(), UserEntry[].class));
+
+        int choices = 0;
 
         while (choices != 3) {
             mainMenuMessage();
@@ -26,34 +27,37 @@ public class Main {
         switch (choices) {
             case 2:
                 List<UserEntry> userEntry1 = new ArrayList<>(userEntries);
-                UserEntry userEntry = new UserEntry();
                 System.out.println("Skriv in en titel");
                 String title = scanner.nextLine();
-                userEntry.setTitle(title);
                 System.out.println("Skriv in ditt inlägg");
                 String text = scanner.nextLine();
-                userEntry.setText(text);
                 String date = LocalDate.now().toString();
-                userEntry.setDate(date);
-
                 UserEntry newUserEntry = new UserEntry(date,text,title);
                 userEntry1.add(newUserEntry);
                 mapper.writeValue(Paths.get("src/main/resources/user.json").toFile(), userEntry1);
-
+                System.out.println(newUserEntry.getDate());
+                System.out.println("Title: "+newUserEntry.getTitle());
+                System.out.println("Text: " +newUserEntry.getText());
                 mainMenuMessage();
-               choices = Integer.parseInt(scanner.nextLine());
-                break;
+                choices = Integer.parseInt(scanner.nextLine());
             case 1:
                 List<UserEntry> userEntriesUpdated = List.of(mapper.readValue(Paths.get("src/main/resources/user.json").toFile(), UserEntry[].class));
-                for (UserEntry userEnter : userEntriesUpdated)
-                    System.out.println(userEnter.getText());
+                for (UserEntry userEnter22 : userEntriesUpdated) {
+                    System.out.println(userEnter22.getDate());
+                    System.out.println("Title: "+userEnter22.getTitle());
+                    System.out.println("Text:" + userEnter22.getText());
+                    System.out.println("-------------------");
+                    mainMenuMessage();
+                }
 
+            }
+            choices = 3;
 
+            }
 
         }
-        }
 
-    }
+
 
 
 
